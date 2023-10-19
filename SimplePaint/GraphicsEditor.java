818d1ab891d4f1da45f1b30e588e2ed4ea6eaa73
M7mdQash <113006736+M7mdQash@users.noreply.github.com>
2023-10-19 11:39:49 +0300

    Added text box, working on shapes dialouge

--- SimplePaint/GraphicsEditor.java
+++ SimplePaint/GraphicsEditor.java
@@ -16,7 +16,7 @@ public class GraphicsEditor {
     private static DrawingPanel drawingPanel;
     private static List<BufferedImage> history = new ArrayList<>();
     private static int historyPointer = -1;
-
+    private Stack<Shape> Shapes;
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             createAndShowGUI();
@@ -26,12 +26,13 @@ public class GraphicsEditor {
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Graphics Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1280, 760);
         frame.setLayout(new BorderLayout());
 
         createMenuBar(frame);
         createDrawingPanel(frame);
         createColorPalette(frame);
+        createShapesPanel(frame);
         createDrawingToolsPanel(frame);
         createAnimationPanel(frame);
 
@@ -53,7 +54,86 @@ public class GraphicsEditor {
         frame.add(drawingPanel, BorderLayout.CENTER);
     }
 
+    /**===========
+     * this area is for the shapes dialouge
+     * ===========
+     * */
+//    private static void showShapesPanel() {
+//        JPanel panel;
+//        if (panel == null) {
+//            panel = createShapesPanel();
+//        }
+//        JOptionPane.showMessageDialog(null, shapesPanel, "Shapes", JOptionPane.PLAIN_MESSAGE);
+//    }
+
+    private static JPanel createShapesPanel(JFrame frame) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        JLabel shapeLabel = new JLabel("Select Shape:");
+        JComboBox<String> shapeComboBox = new JComboBox<>(new String[]{"Rectangle", "Circle", "Triangle", "Polygon", "Line"});
+        panel.add(shapeLabel);
+        panel.add(shapeComboBox);
+
+        JLabel xLabel = new JLabel("X Coordinate:");
+        JTextField xTextField = new JTextField(5);
+        panel.add(xLabel);
+        panel.add(xTextField);
+
+        JLabel yLabel = new JLabel("Y Coordinate:");
+        JTextField yTextField = new JTextField(5);
+        panel.add(yLabel);
+        panel.add(yTextField);
+
+        JLabel sizeLabel = new JLabel("Size:");
+        JTextField sizeTextField = new JTextField(5);
+        panel.add(sizeLabel);
+        panel.add(sizeTextField);
+
+        JButton drawButton = new JButton("Draw");
+        panel.add(drawButton);
+
+        drawButton.addActionListener(e -> {
+
+            String selectedShape = shapeComboBox.getSelectedItem().toString();
+            int x = Integer.parseInt(xTextField.getText());
+            int y = Integer.parseInt(yTextField.getText());
+            int size = Integer.parseInt(sizeTextField.getText());
+
+            drawShape(selectedShape, x, y, size);
+        });
+
+        return panel;
+    }
+
+    private static void drawShape(String shape, int x, int y, int size) {
+        Graphics2D g = (Graphics2D) drawingPanel.getGraphics();
+        g.setColor(currentColor);
+        g.setStroke(new BasicStroke(currentSize));
+
+        if (shape.equals("Rectangle")) {
+            g.drawRect(x, y, size, size);
+        } else if (shape.equals("Circle")) {
+            g.drawOval(x, y, size, size);
+        } else if (shape.equals("Triangle")) {
+            int[] xPoints = {x, x + size / 2, x + size};
+            int[] yPoints = {y + size, y, y + size};
+            g.drawPolygon(xPoints, yPoints, 3);
+        } else if (shape.equals("Polygon")) {
+            // Define the polygon points here
+            int[] xPoints = {x, x + size, x + size / 2};
+            int[] yPoints = {y, y, y + size};
+            g.drawPolygon(xPoints, yPoints, 3);
+        } else if (shape.equals("Line")) {
+            g.drawLine(x, y, x + size, y + size);
+        }
+    }
+
     private static void createColorPalette(JFrame frame) {
+        /**======================
+         * this here creates the color choosing panel
+         * using making an entire new panel
+         * */
         JPanel colorPalettePanel = new JPanel();
         colorPalettePanel.setLayout(new FlowLayout());
 
@@ -87,8 +167,16 @@ public class GraphicsEditor {
 
     private static void createDrawingToolsPanel(JFrame frame) {
         JPanel drawingToolsPanel = new JPanel();
-        drawingToolsPanel.setLayout(new FlowLayout());
+        drawingToolsPanel.setLayout(new GridLayout());
+        drawingToolsPanel.setLayout(new GridLayout(8,0));
+        //using grid layout makes it verticle
 
+        /**===========
+         *
+         * buttons adding section
+         *
+         * ===========
+         * */
         addToolButton(drawingToolsPanel, "Pen", "pen.png");
         addToolButton(drawingToolsPanel, "Eraser", "eraser.png");
         addToolButton(drawingToolsPanel, "Shapes", "shape.png");
@@ -123,7 +211,7 @@ public class GraphicsEditor {
         }
     }
 
-    private static void performAction(String toolName) {
+    private static void performAction(String toolName) {//handels the choice of users
         // Implement the actions associated with each tool here
         switch (toolName) {
             case "Pen":
@@ -134,11 +222,34 @@ public class GraphicsEditor {
                 break;
             case "Clear":
                 clearDrawingPanel();
+                break;
+            case "Text":
+                openTextDialog(); // bug: one text apears and only one that moves on last press and afther writing
+                break;
+//            case "Shapes":
+//                createShapesPanel();
+//                break;
             default:
                 break;
         }
     }
 
+
+    //=========================this method opens the text dialouge to add text on the image
+    private static void openTextDialog() {
+        // Create an instance of the TextDialog class
+        TextDialog textDialog = new TextDialog((Frame) SwingUtilities.getWindowAncestor(drawingPanel));
+
+        // Show the text dialog and get the user's response
+        int userResponse = textDialog.showCustomDialog((Frame) SwingUtilities.getWindowAncestor(drawingPanel));
+
+        // If the user pressed "Apply," update the text properties
+        if (userResponse == TextDialog.APPLY_OPTION) {
+            Font selectedFont = textDialog.getFont();
+            String selectedText = textDialog.getText();
+            drawingPanel.setTextProperties(selectedFont, selectedText);
+        }
+    }
     private static void createAnimationPanel(JFrame frame) {
         JPanel animationPanel = new JPanel();
         JButton createAnimationButton = new JButton("Create Animation Path");
@@ -190,6 +301,28 @@ public class GraphicsEditor {
                 }
             });
         }
+
+        //methods for text editor
+        private Font textFont = new Font("Arial", Font.PLAIN, 20);
+        private String text = "Sample Text";
+
+        public void setTextProperties(Font font, String text) {
+            this.textFont = font;
+            this.text = text;
+            repaint();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            // Draw the text using the selected properties
+            g.setFont(textFont);
+            g.setColor(currentColor);
+            g.drawString(text,lastX , lastY);
+        }
+
+        //==============end for methods of text editor
 
         public void setCurrentTool(Tool tool) {
             currentTool = tool;
