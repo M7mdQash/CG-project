f1555ef348ef6871708eb6c9fb814cd22ab3d2f8
M7mdQash <113006736+M7mdQash@users.noreply.github.com>
2023-10-22 17:55:34 +0300

    2 more things left + text semi fixed

--- SimplePaint/GraphicsEditor.java
+++ SimplePaint/GraphicsEditor.java
@@ -4,19 +4,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+import java.util.Vector;
+import java.awt.geom.AffineTransform;
 
 public class GraphicsEditor {
 
+    private static Vector vector = null;
+
+    private static JPanel canvas;
     private static Color currentColor = Color.BLACK; // Default color
     private static int currentSize = 2; // Default size
     private static DrawingPanel drawingPanel;
     private static List<BufferedImage> history = new ArrayList<>();
     private static int historyPointer = -1;
     private static BufferedImage currentImage;
+
+    //======variable for transformations
+    private static int shapeX = 100;
+    private static int shapeY = 100;
+    private static int shapeSize = 50;
+    private static int rotationAngle = 0; // in radians
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             createAndShowGUI();
@@ -55,6 +67,7 @@ public class GraphicsEditor {
     }
 
 
+
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Graphics Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -70,16 +83,52 @@ public class GraphicsEditor {
 
         frame.setVisible(true);
     }
-
+    //==============
+    //code for saving images
+    //==============
     private static void createMenuBar(JFrame frame) {
         JMenuBar menuBar = new JMenuBar();
+
+        // File Menu
         JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
+
+        // New Option
+        JMenuItem newMenuItem = new JMenuItem("New");
+        newMenuItem.addActionListener(e -> {
+            clearDrawingPanel();
+        });
+        fileMenu.add(newMenuItem);
+
+        // Save Option
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int returnVal = fileChooser.showSaveDialog(frame);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                // Implement your save logic here, using the selected 'file' object.
+                // You can save your drawing data to the chosen file.
+            }
+        });
+        fileMenu.add(saveMenuItem);
+
+        // Open Option
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+            int returnVal = fileChooser.showOpenDialog(frame);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                // Implement your open logic here, using the selected 'file' object.
+                // You can load the drawing data from the chosen file.
+            }
+        });
+        fileMenu.add(openMenuItem);
+
         menuBar.add(fileMenu);
-        menuBar.add(editMenu);
+
         frame.setJMenuBar(menuBar);
     }
-
     private static void createDrawingPanel(JFrame frame) {
         drawingPanel = new DrawingPanel();
         drawingPanel.setBackground(Color.WHITE);
@@ -129,24 +178,238 @@ public class GraphicsEditor {
 
         JRadioButton fillButton = new JRadioButton("Fill");
         panel.add(fillButton);
+        JLabel angleLabel = new JLabel("angle:");
+        JTextField angleTextField = new JTextField(3);
+        panel.add(angleLabel);
+        panel.add(angleTextField);
 
 
+// ================== This part for transformation ===============================
+
+
+
+        JButton moveButton = new JButton("Move");
+        JButton scaleButton = new JButton("Scale");
+        JButton rotateButton = new JButton("Rotate");
+        JButton flipButton = new JButton("Flip");
+
+
+        panel.add(moveButton);
+        panel.add(scaleButton);
+        panel.add(rotateButton);
+        panel.add(flipButton);
+
+
+        //==================
+        //add action to pressing the draw/move/scale etc buttons
+        //==================
+        moveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedShape = shapeComboBox.getSelectedItem().toString();
+                int size = Integer.parseInt(sizeTextField.getText());
+
+                int x = Integer.parseInt(xTextField.getText());
+                int y = Integer.parseInt(yTextField.getText());
+                boolean fill = fillButton.isSelected();
+
+
+                moveShape(selectedShape,x,y,size,fill);
+            }
+        });
+
+        //--------------
+        //scaling button
+        //--------------
+        scaleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedShape = shapeComboBox.getSelectedItem().toString();
+                int x = Integer.parseInt(xTextField.getText());
+                int y = Integer.parseInt(yTextField.getText());
+                int size = Integer.parseInt(sizeTextField.getText());
+                boolean fill = fillButton.isSelected();
+
+                scaleShape(selectedShape,x,y,size,fill);
+            }
+        });
+
+
+        //-------
+        //rotate button
+        //-------
+        rotateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedShape = shapeComboBox.getSelectedItem().toString();
+                int x = Integer.parseInt(xTextField.getText());
+                int y = Integer.parseInt(yTextField.getText());
+                double angle =Double.parseDouble(angleTextField.getText());
+                int size = Integer.parseInt(sizeTextField.getText());
+                boolean fill = fillButton.isSelected();
+
+                rotateShape(selectedShape,angle,size,x,y,fill);
+
+            }
+        });
+
+
+        //======
+        //flip button
+        //======
+        flipButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedShape = shapeComboBox.getSelectedItem().toString();
+                int x = Integer.parseInt(xTextField.getText());
+                int y = Integer.parseInt(yTextField.getText());
+                int size = Integer.parseInt(sizeTextField.getText());
+                boolean fill = fillButton.isSelected();
+
+                fliper(selectedShape,x,y,size,fill);
+            }
+        });
+
+
+        //==============
+        //normal button
+        //==============
         drawButton.addActionListener(e -> {
 
             String selectedShape = shapeComboBox.getSelectedItem().toString();
-
             int x = Integer.parseInt(xTextField.getText());
             int y = Integer.parseInt(yTextField.getText());
             int size = Integer.parseInt(sizeTextField.getText());
             boolean fill = fillButton.isSelected();
+
             drawShape(selectedShape, x, y, size,fill);
+
         });
 
         return panel;
     }
 
 
-    //this method creates shapes in the image
+//    public static void moveShape(String shape,int deltaX, int deltaY,int size,int x ,int y,boolean fill) {
+//        AffineTransform moving = new AffineTransform();
+//        moving.translate(deltaX,deltaY);
+//
+//        drawShape( shape,deltaX, deltaY, size,fill);    }
+
+//    public static void scaleShape(String shape,double scaleFactor,int x,int y,int size,boolean fill) {
+//        size = (int) (size * scaleFactor);
+//
+//        drawShape( shape,x, y, size,fill);
+//    }
+private static void scaleShape(String shape, int x, int y, int size, boolean fill) {
+    int moX = 10 ,  moY = 10;
+    Graphics2D g = (Graphics2D) drawingPanel.getGraphics();
+    g.setColor(currentColor);
+    g.setStroke(new BasicStroke(currentSize));
+    //the scaling is set to 2 for the X and 1.2 for the Y
+    AffineTransform mover = new AffineTransform();
+    mover.scale(2,2);
+
+    g.setTransform(mover);
+
+    if (shape.equals("Rectangle")) {
+        if (fill) g.fillRect(x, y, size, size); // Fill rectangle
+        else g.drawRect(x, y, size, size);
+    } else if (shape.equals("Circle")) {
+        if (fill) g.fillOval(x, y, size, size); // Fill circle
+        else g.drawOval(x, y, size, size);
+    } else if (shape.equals("Triangle")) {
+        int[] xPoints = {x, x + size / 2, x + size};
+        int[] yPoints = {y + size, y, y + size};
+        if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill triangle
+        else g.drawPolygon(xPoints, yPoints, 3);
+
+    } else if (shape.equals("Polygon")) {
+        int[] xPoints = {x, x + size, x + size / 2};
+        int[] yPoints = {y, y, y + size};
+        if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill polygon
+        else g.drawPolygon(xPoints, yPoints, 3);
+    } else if (shape.equals("Line")) {
+        g.drawLine(x, y, x + size, y + size);
+    }
+
+}
+    public static void rotateShape(String shape,double angle , int size , int x ,int y,boolean fill) {
+
+        rotationAngle += (int)angle;
+        drawShape( shape,x, y, rotationAngle,fill);
+    }
+
+//    public static void flipShape(String shape,int x ,int y ,int size,boolean fill) {
+//        size *= -1;
+//        drawShape( shape,x, y, size,fill);
+//    }
+
+    //flip button gives no output
+    private static void fliper(String shape,int x,int y, int size, boolean fill){
+        int moX = 10 ,  moY = 10;
+        Graphics2D g = (Graphics2D) drawingPanel.getGraphics();
+        g.setColor(currentColor);
+        g.setStroke(new BasicStroke(currentSize));
+        AffineTransform mover = new AffineTransform();
+        mover.translate(0, y * 2);
+        g.setTransform(mover);
+
+        if (shape.equals("Rectangle")) {
+            if (fill) g.fillRect(x, y, size, size); // Fill rectangle
+            else g.drawRect(x, y, size, size);
+        } else if (shape.equals("Circle")) {
+            if (fill) g.fillOval(x, y, size, size); // Fill circle
+            else g.drawOval(x, y, size, size);
+        } else if (shape.equals("Triangle")) {
+            int[] xPoints = {x, x + size / 2, x + size};
+            int[] yPoints = {y + size, y, y + size};
+            if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill triangle
+            else g.drawPolygon(xPoints, yPoints, 3);
+
+        } else if (shape.equals("Polygon")) {
+            int[] xPoints = {x, x + size, x + size / 2};
+            int[] yPoints = {y, y, y + size};
+            if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill polygon
+            else g.drawPolygon(xPoints, yPoints, 3);
+        } else if (shape.equals("Line")) {
+            g.drawLine(x, y, x + size, y + size);
+        }
+
+    }
+    private static void moveShape(String shape, int x, int y, int size, boolean fill) {
+        int moX = 10 ,  moY = 10;
+        Graphics2D g = (Graphics2D) drawingPanel.getGraphics();
+        g.setColor(currentColor);
+        g.setStroke(new BasicStroke(currentSize));
+        AffineTransform mover = new AffineTransform();
+        mover.translate(moX,moY);
+        g.setTransform(mover);
+
+        if (shape.equals("Rectangle")) {
+            if (fill) g.fillRect(x, y, size, size); // Fill rectangle
+            else g.drawRect(x, y, size, size);
+        } else if (shape.equals("Circle")) {
+            if (fill) g.fillOval(x, y, size, size); // Fill circle
+            else g.drawOval(x, y, size, size);
+        } else if (shape.equals("Triangle")) {
+            int[] xPoints = {x, x + size / 2, x + size};
+            int[] yPoints = {y + size, y, y + size};
+            if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill triangle
+            else g.drawPolygon(xPoints, yPoints, 3);
+
+        } else if (shape.equals("Polygon")) {
+            int[] xPoints = {x, x + size, x + size / 2};
+            int[] yPoints = {y, y, y + size};
+            if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill polygon
+            else g.drawPolygon(xPoints, yPoints, 3);
+        } else if (shape.equals("Line")) {
+            g.drawLine(x, y, x + size, y + size);
+        }
+
+    }
+
+        //this method creates shapes in the image
     private static void drawShape(String shape, int x, int y, int size, boolean fill) {
         Graphics2D g = (Graphics2D) drawingPanel.getGraphics();
         g.setColor(currentColor);
@@ -155,29 +418,28 @@ public class GraphicsEditor {
         if (shape.equals("Rectangle")) {
             if (fill) g.fillRect(x, y, size, size); // Fill rectangle
             else g.drawRect(x, y, size, size);
-            saveCurrentState();
         } else if (shape.equals("Circle")) {
             if (fill) g.fillOval(x, y, size, size); // Fill circle
             else g.drawOval(x, y, size, size);
-            saveCurrentState();
         } else if (shape.equals("Triangle")) {
             int[] xPoints = {x, x + size / 2, x + size};
             int[] yPoints = {y + size, y, y + size};
             if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill triangle
             else g.drawPolygon(xPoints, yPoints, 3);
-            saveCurrentState();
+
         } else if (shape.equals("Polygon")) {
             int[] xPoints = {x, x + size, x + size / 2};
             int[] yPoints = {y, y, y + size};
             if (fill) g.fillPolygon(xPoints, yPoints, 3); // Fill polygon
             else g.drawPolygon(xPoints, yPoints, 3);
-            saveCurrentState();
         } else if (shape.equals("Line")) {
             g.drawLine(x, y, x + size, y + size);
-            saveCurrentState();
         }
 
     }
+    //-------------------------------------------------------------
+    //                  end of section for shapes
+    //-------------------------------------------------------------
 
     private static void createColorPalette(JFrame frame) {
         /**======================
@@ -230,7 +492,7 @@ public class GraphicsEditor {
         addToolButton(drawingToolsPanel, "Pen", "pen.png");
         addToolButton(drawingToolsPanel, "Eraser", "eraser.png");
         addToolButton(drawingToolsPanel, "Shapes", "shape.png");
-        addToolButton(drawingToolsPanel, "Fill", "color.png");
+        addToolButton(drawingToolsPanel, "Vector", "point-transformation.png");
         addToolButton(drawingToolsPanel, "Text", "text.png");
         addToolButton(drawingToolsPanel, "Undo", "undo.png");
         addToolButton(drawingToolsPanel, "Redo", "redo.png");
@@ -282,25 +544,41 @@ public class GraphicsEditor {
             case "Fill":
                 break;
             case "Undo":
-                if (historyPointer > 0) {
-                    historyPointer--;
-                    currentImage = history.get(historyPointer);
-                    drawingPanel.repaint();
-                }
+
                 break;
             case "Redo":
-                if (historyPointer < history.size() - 1) {
-                    historyPointer++;
-                    currentImage = history.get(historyPointer);
-                    drawingPanel.repaint();
-                }
-                break;
 
+                break;
+            case "Vector":
+                startVector();
+                break;
             default:
                 break;
         }
     }
 
+
+    //=======
+    //method for vectors
+    //=======
+    private static void startVector() {
+
+            // Prompt the user for the destination point (x, y)
+            int x = Integer.parseInt(JOptionPane.showInputDialog("Enter the X coordinate:"));
+            int y = Integer.parseInt(JOptionPane.showInputDialog("Enter the Y coordinate:"));
+
+            vector = new Vector(x, y);
+            drawVector(vector.x, vector.y);
+
+    }
+    private static void drawVector(int x, int y) {
+        Graphics2D g = (Graphics2D) drawingPanel.getGraphics();
+        g.setColor(currentColor);
+        g.setStroke(new BasicStroke(currentSize));
+        g.drawLine(0, 0, x, y);
+    }
+    //end methods for vectors
+    //========================
 
     //=========================this method opens the text dialouge to add text on the image
     private static void openTextDialog() {
@@ -315,9 +593,7 @@ public class GraphicsEditor {
             Font selectedFont = textDialog.getFont();
             String selectedText = textDialog.getText();
             drawingPanel.setTextProperties(selectedFont, selectedText);
-            saveCurrentState();
         }
-
     }
     private static void createAnimationPanel(JFrame frame) {
         JPanel animationPanel = new JPanel();
@@ -392,7 +668,7 @@ public class GraphicsEditor {
         public void setTextProperties(Font font, String text) {
             this.textFont = font;
             this.text = text;
-            repaint();
+            drawingPanel.repaint();
         }
 
         @Override
@@ -417,6 +693,17 @@ public class GraphicsEditor {
 
         enum Tool {
             PEN, ERASER, SHAPES, FILL, TEXT, UNDO, REDO
+        }
+
+    }
+    // Add a Vector object to track the vector's destination point
+    static class Vector {
+        int x;
+        int y;
+
+        public Vector(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
